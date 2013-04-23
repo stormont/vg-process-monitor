f28ee6a70fdc94cf3d49c86bec9065b47fd2a530
Devan Stormont <stormont@gmail.com>
2013-04-23 00:21:51 -0700

    Added time interval handling for log/data workers.

--- System/Process/Monitor.hs
+++ System/Process/Monitor.hs
@@ -3,6 +3,8 @@ module System.Process.Monitor
    where
 
 import qualified Data.ByteString.Lazy.Char8 as B
+import Control.Concurrent (threadDelay)
+import Data.Time.Clock
 import System.Exit
 import System.Process
 
@@ -30,31 +32,69 @@ data Job = Job
    } deriving (Show,Read)
 
 
-type Monitor = [Job]
+-- Intervals at which to run log/data backups, in seconds
+data Intervals = Intervals
+   { lastTime     :: UTCTime
+   , logInterval  :: NominalDiffTime
+   , dataInterval :: NominalDiffTime
+   }
 
 
-doWork :: Job -> IO ()
-doWork job = do
-   putStrLn "Hello"
-   launchWorker job
-   putStrLn "Done"
+launchWorker :: Job -> Intervals -> IO ()
+launchWorker job intervals = do
+   h <- startWorker $ jobWorker job
+   monitorWorker job h intervals
 
 
-launchWorker :: Job -> IO ()
-launchWorker job = do
-   let worker = jobWorker job
+startWorker :: Executable -> IO (ProcessHandle)
+startWorker worker = do
    (_, _, _, h) <- createProcess (proc (B.unpack $ execCmd worker) (map (B.unpack) $ execArgs worker))
                                  { std_out = CreatePipe
                                  , std_in  = CreatePipe
                                  , std_err = CreatePipe
                                  }
-   exitCode <- waitForProcess h
+   return h
+
+
+monitorWorker :: Job -> ProcessHandle -> Intervals -> IO ()
+monitorWorker job workerHandle intervals = do
+   threadDelay 100000  -- 100 ms delay
+   exitCode <- getProcessExitCode workerHandle
    case exitCode of
-      ExitSuccess   -> return ()
-      ExitFailure c -> do
-         performRecovery
-         launchWorker job
+      Nothing -> do
+         runWorkerIfReady job intervals dataInterval runDataWorker
+         runWorkerIfReady job intervals logInterval  runLogWorker
+         curTime <- getCurrentTime
+         monitorWorker job workerHandle (intervals { lastTime = curTime })
+      Just ExitSuccess     -> do
+         runDataWorker job
+         runLogWorker job
+      Just (ExitFailure c) -> do
+         performRecovery job
+         putStrLn "Relaunching worker..."
+         curTime <- getCurrentTime
+         launchWorker job (intervals { lastTime = curTime })
 
 
-performRecovery = do
-   putStrLn "Does some recovery tasks..."
+runWorkerIfReady :: Job -> Intervals -> (Intervals -> NominalDiffTime) -> (Job -> IO ()) -> IO ()
+runWorkerIfReady job intervals extractFunc runFunc = do
+   curTime <- getCurrentTime
+   let diffTime = diffUTCTime curTime (lastTime intervals)
+   if diffTime > (extractFunc intervals)
+      then runFunc job
+      else return ()
+
+
+runDataWorker :: Job -> IO ()
+runDataWorker job = putStrLn "Ran data worker"
+
+
+runLogWorker :: Job -> IO ()
+runLogWorker job = putStrLn "Ran log worker"
+
+
+performRecovery :: Job -> IO ()
+performRecovery job = do
+   putStrLn "Recovering data..."
+   runDataWorker job
+   runLogWorker job
--- procmon.hs
+++ procmon.hs
@@ -3,11 +3,16 @@ module Main
    where
 
 import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Time.Clock
 
 import System.Process.Monitor
 
 
 main = do
+   time <- getCurrentTime
    let worker = Executable (B.pack "temp.exe") []
        job = Job worker Nothing Nothing Nothing Nothing
-   doWork job
+       intervals = Intervals time 0 0
+   putStrLn "Monitoring beginning..."
+   launchWorker job intervals
+   putStrLn "Monitoring complete"
