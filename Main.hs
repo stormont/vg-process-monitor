ce8dce38702a9fe2399260f77eccbe8d6ab4ebe9
stormont <stormont@gmail.com>
2015-04-27 23:57:59 -0700

    Modifications to process monitor to allow better control over terminating the monitor and child - still a work in progress

---
+++ Main.hs
@@ -0,0 +1,29 @@
+
+module Main
+   where
+
+import Control.Concurrent (forkIO)
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TVar
+import Control.Monad.Trans (liftIO)
+import Data.Time.Clock
+import System.Environment (getArgs)
+
+import System.Process.Monitor
+
+
+main = do
+   args <- getArgs
+   putStrLn "Monitoring beginning..."
+   time <- getCurrentTime
+   comm <- liftIO $ atomically $ newTVar $ mkComm time
+   doWork args comm time
+   putStrLn "Monitoring complete"
+
+
+doWork args comm time = do
+   let worker = Executable (head args) []
+       job = mkJob worker
+       intervals = Intervals time time 3 1
+   _ <- forkIO $ launchWorker job comm intervals
+   return ()
--- System/Process/Monitor.hs
+++ System/Process/Monitor.hs
@@ -2,8 +2,10 @@
 module System.Process.Monitor
    where
 
-import qualified Data.ByteString.Lazy.Char8 as B
 import Control.Concurrent (threadDelay)
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TVar
+import Control.Monad.Trans (liftIO)
 import Data.Time.Clock
 import System.Exit
 import System.Process
@@ -12,8 +14,8 @@ import System.Process
 -- TODO Settings for stdout/stdin/stderr?
 
 data Executable = Executable
-   { execCmd  :: B.ByteString
-   , execArgs :: [B.ByteString]
+   { execCmd  :: String
+   , execArgs :: [String]
    } deriving (Show,Read)
 
 
@@ -21,9 +23,10 @@ data Job = Job
    { jobWorker    :: Executable
    , jobLog       :: Maybe Executable
    , jobData      :: Maybe Executable
-   , jobCleanStop :: Maybe Executable
-   , jobUpgrade   :: Maybe Executable
    } deriving (Show,Read)
+
+
+mkJob worker = Job worker Nothing Nothing
 
 
 -- Intervals at which to run log/data backups, in seconds
@@ -35,18 +38,30 @@ data Intervals = Intervals
    }
 
 
+data Comm = Comm
+  { commLastStart :: UTCTime
+  , commNumFailures :: Int
+  , commTerminate :: Bool
+  } deriving (Show,Read)
+
+
+mkComm :: UTCTime -> Comm
+mkComm time = Comm time 0 False
+
+
 launchWorker :: Job
+             -> TVar Comm
              -> Intervals
              -> IO ()
-launchWorker job intervals = do
+launchWorker job comm intervals = do
    h <- startWorker $ jobWorker job
-   monitorWorker job h intervals
+   monitorWorker job comm intervals h
 
 
 startWorker :: Executable
             -> IO (ProcessHandle)
 startWorker worker = do
-   (_, _, _, h) <- createProcess (proc (B.unpack $ execCmd worker) (map (B.unpack) $ execArgs worker))
+   (_, _, _, h) <- createProcess (proc (execCmd worker) (execArgs worker))
                                  { std_out = Inherit
                                  , std_in  = Inherit
                                  , std_err = Inherit
@@ -54,26 +69,50 @@ startWorker worker = do
    return h
 
 
-monitorWorker :: Job
-              -> ProcessHandle
-              -> Intervals
+reportResults :: TVar Comm
               -> IO ()
-monitorWorker job workerHandle intervals = do
+reportResults comm = do
+   comm' <- liftIO $ atomically $ readTVar comm
+   putStrLn $ "Last Time: " ++ show (commLastStart comm')
+   putStrLn $ "Failures: " ++ show (commNumFailures comm')
+
+
+monitorWorker :: Job
+              -> TVar Comm
+              -> Intervals
+              -> ProcessHandle
+              -> IO ()
+monitorWorker job comm intervals workerHandle = do
    threadDelay 100000  -- 100 ms delay
    exitCode <- getProcessExitCode workerHandle
    case exitCode of
       Nothing -> do
          intervals'  <- checkDataInterval job intervals
          intervals'' <- checkLogInterval  job intervals'
-         monitorWorker job workerHandle intervals''
+         comm' <- liftIO $ atomically $ readTVar comm
+         if commTerminate comm'
+            then do
+               putStrLn "Terminating..."
+               terminateProcess workerHandle
+               reportResults comm
+            else monitorWorker job comm intervals'' workerHandle
       Just ExitSuccess     -> do
          runDataWorker job
          runLogWorker job
       Just (ExitFailure c) -> do
          performRecovery job
-         putStrLn "Relaunching worker..."
-         curTime <- getCurrentTime
-         launchWorker job (intervals { lastLog = curTime, lastData = curTime })
+         comm' <- liftIO $ atomically $ readTVar comm
+         if commTerminate comm'
+            then do
+               putStrLn "Terminating..."
+               reportResults comm
+            else do
+               putStrLn "Relaunching worker..."
+               curTime <- getCurrentTime
+               putStrLn $ "Time: " ++ show curTime
+               putStrLn $ "NumFailures: " ++ show ((commNumFailures comm') + 1)
+               liftIO $ atomically $ modifyTVar comm (\x -> x { commLastStart = curTime, commNumFailures = (commNumFailures x) + 1 })
+               launchWorker job comm (intervals { lastLog = curTime, lastData = curTime })
 
 
 checkDataInterval :: Job
@@ -117,7 +156,7 @@ runWorker extractFunc desc job = do
    case extractFunc job of
       Nothing -> return ()
       Just j  -> do
-         _ <- createProcess (proc (B.unpack $ execCmd j) (map (B.unpack) $ execArgs j))
+         _ <- createProcess (proc (execCmd j) (execArgs j))
                             { std_out = Inherit
                             , std_in  = Inherit
                             , std_err = Inherit
